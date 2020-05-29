499effb00ae31b17034f44273a798412fb2fb432
Joydeep Biswas <joydeepb@cs.utexas.edu>
2020-05-29 17:43:52 -0500

    Working build

---
+++ src/ekf_tracker/ekf_tracker_main.cc
@@ -0,0 +1,107 @@
+// Copyright (C) 2020 Joydeep Biswas
+// University of Texas at Austin
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+// ========================================================================
+
+#include <signal.h>
+#include <stdlib.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <inttypes.h>
+#include <vector>
+
+#include "glog/logging.h"
+#include "gflags/gflags.h"
+#include "eigen3/Eigen/Dense"
+#include "eigen3/Eigen/Geometry"
+#include "gflags/gflags.h"
+#include "geometry_msgs/Pose2D.h"
+#include "geometry_msgs/PoseArray.h"
+#include "geometry_msgs/PoseStamped.h"
+#include "geometry_msgs/PoseWithCovarianceStamped.h"
+#include "sensor_msgs/LaserScan.h"
+#include "visualization_msgs/Marker.h"
+#include "visualization_msgs/MarkerArray.h"
+#include "nav_msgs/Odometry.h"
+#include "ros/ros.h"
+#include "shared/math/math_util.h"
+#include "shared/util/timer.h"
+#include "shared/ros/ros_helpers.h"
+
+#include "ekf_tracker/ekf_tracker.h"
+
+using math_util::DegToRad;
+using math_util::RadToDeg;
+using ros_helpers::Eigen3DToRosPoint;
+using ros_helpers::Eigen2DToRosPoint;
+using ros_helpers::RosPoint;
+using ros_helpers::SetRosVector;
+using std::string;
+using std::vector;
+using Eigen::Vector2f;
+
+// Create command line arguments
+DEFINE_string(laser_topic, "scan", "Name of ROS topic for LIDAR data");
+DEFINE_string(odom_topic, "odom", "Name of ROS topic for odometry data");
+
+bool run_ = true;
+
+void LaserCallback(const sensor_msgs::LaserScan& msg) {
+  if (FLAGS_v > 0) {
+    printf("Laser t=%f, dt=%f\n",
+           msg.header.stamp.toSec(),
+           GetWallTime() - msg.header.stamp.toSec());
+  }
+}
+
+void OdometryCallback(const nav_msgs::Odometry& msg) {
+  if (FLAGS_v > 0) {
+    printf("Odometry t=%f\n", msg.header.stamp.toSec());
+  }
+}
+
+void SignalHandler(int) {
+  if (!run_) {
+    printf("Force Exit.\n");
+    exit(0);
+  }
+  printf("Exiting.\n");
+  run_ = false;
+}
+
+int main(int argc, char** argv) {
+  google::ParseCommandLineFlags(&argc, &argv, false);
+  signal(SIGINT, SignalHandler);
+  // Initialize ROS.
+  ros::init(argc, argv, "dynamic_tracker", ros::init_options::NoSigintHandler);
+  ros::NodeHandle n;
+
+  ros::Subscriber odom_sub =
+      n.subscribe(FLAGS_odom_topic, 1, &OdometryCallback);
+  ros::Subscriber laser_sub =
+      n.subscribe(FLAGS_laser_topic, 1, &LaserCallback);
+
+  RateLoop loop(20.0);
+  while (run_ && ros::ok()) {
+    ros::spinOnce();
+    loop.Sleep();
+  }
+  return 0;
+}
