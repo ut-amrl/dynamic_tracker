42a5a2d14c245cbfaf1afc7bfefd74d98e4befbe
Shikhar Gupta <shikhar.gupta.tx@gmail.com>
2020-06-17 18:27:24 -0500

    Added some diagnostic info about recording

--- k4a_image/main.cpp
+++ k4a_image/main.cpp
@@ -12,16 +12,20 @@ int main() {
         return 1;
     }
     uint64_t recording_length = k4a_playback_get_last_timestamp_usec(playback_handle);
-    printf("Recording is %lld seconds long\n", recording_length / 1000000);
 
     // read captures from mkv file
     k4a_capture_t capture = NULL;
     k4a_stream_result_t result = K4A_STREAM_RESULT_SUCCEEDED;
+
+    // count frames
+    int count = 0;
     while (result == K4A_STREAM_RESULT_SUCCEEDED) {
         result = k4a_playback_get_next_capture(playback_handle, &capture);
         if (result == K4A_STREAM_RESULT_SUCCEEDED){
+            count++;
             // get image from capture and print image info
             k4a_image_t image = k4a_capture_get_color_image(capture);
+            printf("%d", K4A_IMAGE_FORMAT_COLOR_MJPG == k4a_image_get_format(image));
             printf(" | Depth16 res:%4dx%4d stride:%5d\n",
                 k4a_image_get_height_pixels(image),
                 k4a_image_get_width_pixels(image),
@@ -29,16 +33,27 @@ int main() {
             k4a_image_release(image);
             k4a_capture_release(capture);
         }
+
+        // reached eof
         else if (result == K4A_STREAM_RESULT_EOF) {
             printf("Read entire recording!\n");
             break;
         }
     }
+
+    // failed to stream
     if (result == K4A_STREAM_RESULT_FAILED) {
    	k4a_playback_close(playback_handle);
         printf("Failed to read entire recording\n");
         return 1;
     }
+
+    // print some recording info
+    printf("Recording is %d seconds long\n", recording_length / 1000000);
+    printf("Total Frames Read: %d\n", count);
+    printf("Average frames per second: %d\n",  count / (recording_length / 1000000));
+
+    // exit
     k4a_playback_close(playback_handle);
     return 0;
 }
