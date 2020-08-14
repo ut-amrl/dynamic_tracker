94de9e0e2e1c3785a8da617edc986d0f70b3d3b7
Henry Liu <henry.liu.texas@gmail.com>
2020-08-14 12:18:48 -0500

    Add body tracker processing

--- k4a_image/include/KFRBodyTracker.h
+++ k4a_image/include/KFRBodyTracker.h
@@ -8,11 +8,12 @@
 
 class KFRBodyTracker : public K4ACaptureRecipient {
 public:
-    KFRBodyTracker(k4a_calibration_t sensor_calibration);
+    KFRBodyTracker(k4a_calibration_t sensor_calibration, bool realTime = true);
     ~KFRBodyTracker();
     void receiveFrame(k4a_capture_t capture);
 protected:
     k4abt_tracker_t _tracker;
+    bool _realTime;
 };
 
 #endif
--- k4a_image/src/KFRBodyTracker.cpp
+++ k4a_image/src/KFRBodyTracker.cpp
@@ -1,15 +1,34 @@
 #include "KFRBodyTracker.h"
 
-KFRBodyTracker::KFRBodyTracker(k4a_calibration_t sensor_calibration)
+KFRBodyTracker::KFRBodyTracker(k4a_calibration_t sensor_calibration, bool realTime)
+    : _realTime(realTime)
 {
     k4abt_tracker_configuration_t tracker_config = K4ABT_TRACKER_CONFIG_DEFAULT;
     k4abt_tracker_create(&sensor_calibration, tracker_config, &_tracker);
 }
 
-KFRBodyTracker::~KFRBodyTracker() {}
+KFRBodyTracker::~KFRBodyTracker()
+{
+    k4abt_tracker_destroy(_tracker);
+}
 
 void KFRBodyTracker::receiveFrame(k4a_capture_t capture)
 {
     k4a_capture_reference(capture);
+    // Process real time or synchronously
+    int wait = _realTime ? 0 : K4A_WAIT_INFINITE;
+    k4a_wait_result_t queue_capture_result = k4abt_tracker_enqueue_capture(_tracker, capture, wait);
     k4a_capture_release(capture);
+    if (queue_capture_result != K4A_WAIT_RESULT_SUCCEEDED) {
+        // It should never hit timeout or error when K4A_WAIT_INFINITE is set.
+        printf("Error! Adding capture to tracker process queue failed!\n");
+        return;
+    }
+    k4abt_frame_t body_frame = NULL;
+    k4a_wait_result_t pop_frame_result = k4abt_tracker_pop_result(_tracker, &body_frame, wait);
+    if (pop_frame_result != K4A_WAIT_RESULT_SUCCEEDED) {
+        // It should never hit timeout or error when K4A_WAIT_INFINITE is set.
+        printf("Error! Popping body tracking result failed!\n");
+        return;
+    }
 }
