ac9971a31a07110593b20a5bf0dc365b9e7953d0
Jonathan Browne <12983479+jbyoshi@users.noreply.github.com>
2020-10-15 16:42:08 -0500

    Spinning teapots in GLFW

--- k4a_image/examples/glfw/src/main.cpp
+++ k4a_image/examples/glfw/src/main.cpp
@@ -1,33 +1,8 @@
 #include <GLFW/glfw3.h>
-
-int main(void) {
-    if (!glfwInit())
-        return -1;
-
-    GLFWwindow *window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window) {
-        glfwTerminate();
-        return -1;
-    }
-
-    glfwMakeContextCurrent(window);
-
-    while (!glfwWindowShouldClose(window)) {
-        glClear(GL_COLOR_BUFFER_BIT);
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-    }
-
-    glfwTerminate();
-    return 0;
-}
-
-/*
-
-http://math.hws.edu/bridgeman/courses/324/s06/doc/opengl.html
-
 #include <GL/gl.h>
 #include <GL/glut.h>
+
+#include <iostream>
 
 void display () {
 
@@ -53,7 +28,99 @@ void display () {
 
 }
 
-void reshape ( int width, int height ) {
+static void error_callback(int error, const char* description)
+{
+    fprintf(stderr, "Error: %s\n", description);
+}
+
+int main(int argc, char** argv) {
+        if (!glfwInit())
+        return -1;
+        glutInit(&argc, argv);
+
+    glfwSetErrorCallback(error_callback);
+    if (!glfwInit())
+        exit(EXIT_FAILURE);
+
+    GLFWwindow *window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    // glViewport(0, 0,  640, 480);
+    if (!window) {
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1);
+
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    //gluPerspective(60,1,1,10);
+
+    const GLdouble pi = 3.1415926535897932384626433832795;
+    GLdouble fW, fH;
+
+    //fH = tan( (fovY / 2) / 180 * pi ) * zNear;
+    fH = 0.57;
+    fW = fH * 1;
+
+    glFrustum( -fW, fW, -fH, fH, 1, 100);
+
+
+    double angle = 0;
+    while (!glfwWindowShouldClose(window)) {
+        glMatrixMode(GL_MODELVIEW);
+        glLoadIdentity();
+        glTranslatef(0, 0, -7.5);
+        glRotatef(angle, 0, 1, 0);
+        angle += 1;
+
+        glViewport(0,0,640,480);
+        display();
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
+
+
+
+// int main(int argc, char **argv) {
+//     if (!glfwInit())
+//         return -1;
+//     glutInit(&argc, argv);
+
+//     GLFWwindow *window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+//     // glViewport(0, 0,  640, 480);
+//     if (!window) {
+//         glfwTerminate();
+//         return -1;
+//     }
+
+//     glfwMakeContextCurrent(window);
+
+//     glMatrixMode(GL_PROJECTION);
+//     glLoadIdentity();
+
+//     while (!glfwWindowShouldClose(window)) {
+//         glViewport(0,0,640,480);
+//         display();
+//         glfwSwapBuffers(window);
+//         glfwPollEvents();
+//     }
+
+//     glfwTerminate();
+//     return 0;
+// }
+
+
+
+
+//http://math.hws.edu/bridgeman/courses/324/s06/doc/opengl.html
+
+
+/*void reshape ( int width, int height ) {
     glViewport(0,0,width,height);
 
 }
@@ -79,5 +146,4 @@ int main ( int argc, char * argv[] ) {
     gluLookAt(0.0,0.0,5.0,0.0,0.0,0.0,0.0,1.0,0.0);
 
     glutMainLoop();
-}
-*/
+}*/
